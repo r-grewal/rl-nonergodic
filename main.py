fe2895a9a9659685c456cf414394016c022cefbe
r-grewal <79781370+r-grewal@users.noreply.github.com>
2021-03-02 02:27:19 +1100

    added environments and mkdir for results

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import gym
 import pybullet_envs
 import numpy as np
@@ -9,42 +10,44 @@ from utils import plot_learning_curve
 # 'LunarLanderContinuous-v2', 'BipedalWalker-v3', 'BipedalWalkerHardcore-v3'
 
 # PyBullet gym environments 
-# 'CartPoleContinuousBulletEnv-v0', 'InvertedPendulumBulletEnv-v0', 'Walker2DBulletEnv-v0',
+# 'CartPoleContinuousBulletEnv-v0', 'InvertedPendulumBulletEnv-v0','InvertedDoublePendulumBulletEnv-v0',      
+# 'KukaBulletEnv-v0', 'HopperBulletEnv-v0', 'Walker2DBulletEnv-v0',
 # 'HalfCheetahBulletEnv-v0', 'AntBulletEnv-v0', 'HumanoidBulletEnv-v0'
 
 if __name__ == '__main__':
 
     envs = ['LunarLanderContinuous-v2', 'BipedalWalker-v3', 'BipedalWalkerHardcore-v3',
-            'CartPoleContinuousBulletEnv-v0', 'InvertedPendulumBulletEnv-v0', 'Walker2DBulletEnv-v0',
+            'CartPoleContinuousBulletEnv-v0', 'InvertedPendulumBulletEnv-v0','InvertedDoublePendulumBulletEnv-v0',      
+            'KukaBulletEnv-v0', 'HopperBulletEnv-v0', 'Walker2DBulletEnv-v0',
             'HalfCheetahBulletEnv-v0', 'AntBulletEnv-v0', 'HumanoidBulletEnv-v0']
 
-    env_id = envs[0]
+    env_id = envs[0]    # select environment
 
     env = gym.make(env_id)
     input_dims = env.observation_space.shape
     num_actions = env.action_space.shape[0]
+    env._max_episode_steps = 1e6
 
-    if env_id == envs[0] or env_id == envs[1] or env_id == envs[2]:
-        env._max_episode_steps = 2500
-
-    algo = 'TD3'            # set to 'TD3' or 'SAC'
+    algo = 'SAC'            # set to 'TD3' or 'SAC'
     loss_fn = 'MSE'         # set to 'Cauchy', 'HSC', 'Huber', 'MAE', 'MSE' or 'TCauchy'
     random_steps = 1000     # intial random steps for TD3
+    scale = 1               # reward scale for SAC
     buffer = 1e6            # size of experience replay buffer
 
     if algo == 'TD3':
         agent = Agent_td3(env_id, env, input_dims, num_actions, lr_alpha=0.001, lr_beta=0.001, tau=0.005, 
                 gamma=0.99, actor_update_interval=2, warmup=random_steps, max_size=buffer, layer1_dim=400, 
                 layer2_dim=300, batch_size=100, policy_noise=0.1, target_policy_noise=0.2, 
-                target_policy_clip=0.5, loss_type=loss_fn, cauchy_scale=0.420, algo_name=algo)
+                target_policy_clip=0.5,loss_type=loss_fn, cauchy_scale=0.420, algo_name=algo)
 
     elif algo == 'SAC':
         agent = Agent_sac(env_id, env, input_dims, num_actions, lr_alpha=3e-4, lr_beta=3e-4, lr_kappa=3e-4, 
                 tau=0.005, gamma=0.99, actor_update_interval=1, max_size=buffer, layer1_dim=256, layer2_dim=256, 
-                batch_size=256, reparam_noise=1e-6, reward_scale=1, loss_type =loss_fn, cauchy_scale=0.420, algo_name=algo)
+                batch_size=256, reparam_noise=1e-6, reward_scale=scale, loss_type =loss_fn, cauchy_scale=0.420, algo_name=algo)
 
-    n_rounds = 1            # number of trials
-    n_games = 1000          # number of episodes per trial
+    n_rounds = 1    # number of trials
+    n_steps = 3e6    # maximum cumulative steps per round
+    n_games = 300    # maximum number of episodes per round
 
     directory = 'results/'+env_id+'/'+env_id+'_'+algo+'_'+loss_fn+'_'+'e'+str(n_games)+'_'+'n'
 
@@ -59,13 +62,17 @@ if __name__ == '__main__':
         if algo == 'SAC':
             logtemp_history = np.zeros((n_games,))
 
+        cum_steps = 0
+
+        # agent.load_models()
+
+        # while cum_steps < n_steps:
         for i in range(n_games):
             state = env.reset()
             done = False
 
             score = 0
             step = 0
-            # agent.load_models()
 
             while not done:
                 # action = env.action_space.sample()
@@ -77,6 +84,7 @@ if __name__ == '__main__':
 
                 score += reward
                 step += 1
+                cum_steps += 1
                 # print(step)
                 # env.render(mode='human')
 
@@ -89,8 +97,8 @@ if __name__ == '__main__':
             if algo == 'SAC':
                 logtemp_history[i] = logtemp
 
-            print('epis/step {}/{}: score {:1.1f}, trail {:1.1f}, C/A loss {:1.1f}/{:1.1f}'
-                .format(i+1, step, score, trail_score, q1_loss+q2_loss, actor_loss))
+            print('epis/step/tot {}/{}/{}: score {:1.1f}, trail {:1.1f}, C/A loss {:1.1f}/{:1.1f}'
+                .format(i+1, step, cum_steps, score, trail_score, q1_loss+q2_loss, actor_loss))
 
             if trail_score > best_score:
                 best_score = trail_score
@@ -100,8 +108,11 @@ if __name__ == '__main__':
         filename_npy_step = directory + str(itr+1)+'_step'+'.npy'
         filename_npy_loss = directory + str(itr+1)+'_loss'+'.npy'
         filename_npy_scale = directory + str(itr+1)+'_scale'+'.npy'
-        filename_npy_logtemp = directory + str(itr+1)+'_temp'+'.npy'
+        filename_npy_logtemp = directory + str(itr+1)+'_logtemp'+'.npy'
         filename_png = directory + str(itr+1)+'.png'
+
+        if not os.path.exists('./results/'+env_id):
+            os.makedirs('./results/'+env_id)
         
         score_history = np.array(score_history)
         np.save(filename_npy_score, score_history)
