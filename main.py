05aac7e8e2c30ee25a4b079e178f1b95ad98ea19
r-grewal <79781370+r-grewal@users.noreply.github.com>
2021-03-05 13:44:03 +1100

    restructured for future updates

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 import gym
 import pybullet_envs
 import numpy as np
@@ -6,68 +7,60 @@ from algo_td3 import Agent_td3
 from algo_sac import Agent_sac
 from utils import plot_learning_curve
 
-# OpenAI gym environments
-# 'LunarLanderContinuous-v2', 'BipedalWalker-v3', 'BipedalWalkerHardcore-v3'
+algo = 'TD3'            # set to 'TD3' or 'SAC'
+loss_fn = 'MSE'         # set to 'Cauchy', 'HSC', 'Huber', 'MAE', 'MSE' or 'TCauchy'
+multi_steps = 1        # set bootstrapping of target values
+ergodicity = 'yes'      # whether to assume ergodicity
+buffer = 1e6            # size of experience replay buffer
+discount = 0.99         # discount rate per step
+random_steps = 1000     # intial random steps for TD3
+scale = 1               # reward scale for SAC
+sdist = 'mvn'           # set stochastic sampling 'beta', 'mvn', 'st' or 'uvn' for SAC
 
-# PyBullet gym environments 
-# 'CartPoleContinuousBulletEnv-v0', 'InvertedPendulumBulletEnv-v0','InvertedDoublePendulumBulletEnv-v0',      
-# 'KukaBulletEnv-v0', 'HopperBulletEnv-v0', 'Walker2DBulletEnv-v0',
-# 'HalfCheetahBulletEnv-v0', 'AntBulletEnv-v0', 'HumanoidBulletEnv-v0'
+n_trials = 1            # number of total trials
+n_episodes = 500      # maximum number of episodes per trial
+n_cumsteps = 3e6        # maximum cumulative steps per trial regardless of episodes
 
-if __name__ == '__main__':
-
-    envs = ['LunarLanderContinuous-v2', 'BipedalWalker-v3', 'BipedalWalkerHardcore-v3',
-            'CartPoleContinuousBulletEnv-v0', 'InvertedPendulumBulletEnv-v0','InvertedDoublePendulumBulletEnv-v0',      
+openai = ['LunarLanderContinuous-v2', 'BipedalWalker-v3', 'BipedalWalkerHardcore-v3']
+pybullet = ['CartPoleContinuousBulletEnv-v0', 'InvertedPendulumBulletEnv-v0','InvertedDoublePendulumBulletEnv-v0',      
             'KukaBulletEnv-v0', 'HopperBulletEnv-v0', 'Walker2DBulletEnv-v0',
             'HalfCheetahBulletEnv-v0', 'AntBulletEnv-v0', 'HumanoidBulletEnv-v0']
 
-    env_id = envs[0]    # select environment
+if __name__ == '__main__':
 
+    env_id = pybullet[2]
     env = gym.make(env_id)
+    env = env.unwrapped
+    # env._max_episode_steps = 5000
     input_dims = env.observation_space.shape
     num_actions = env.action_space.shape[0]
-    env._max_episode_steps = 1e6
-
-    algo = 'SAC'            # set to 'TD3' or 'SAC'
-    loss_fn = 'MSE'         # set to 'Cauchy', 'HSC', 'Huber', 'MAE', 'MSE' or 'TCauchy'
-    random_steps = 1000     # intial random steps for TD3
-    scale = 1               # reward scale for SAC
-    buffer = 1e6            # size of experience replay buffer
 
     if algo == 'TD3':
+        batch_size = 100
         agent = Agent_td3(env_id, env, input_dims, num_actions, lr_alpha=0.001, lr_beta=0.001, tau=0.005, 
                 gamma=0.99, actor_update_interval=2, warmup=random_steps, max_size=buffer, layer1_dim=400, 
-                layer2_dim=300, batch_size=100, policy_noise=0.1, target_policy_noise=0.2, 
-                target_policy_clip=0.5,loss_type=loss_fn, cauchy_scale=0.420, algo_name=algo)
+                layer2_dim=300, batch_size=batch_size, policy_noise=0.1, target_policy_noise=0.2, 
+                target_policy_clip=0.5,loss_type=loss_fn, cauchy_scale=0.420, algo_name=algo, erg=ergodicity)
 
     elif algo == 'SAC':
+        batch_size = 256
         agent = Agent_sac(env_id, env, input_dims, num_actions, lr_alpha=3e-4, lr_beta=3e-4, lr_kappa=3e-4, 
                 tau=0.005, gamma=0.99, actor_update_interval=1, max_size=buffer, layer1_dim=256, layer2_dim=256, 
-                batch_size=256, reparam_noise=1e-6, reward_scale=scale, loss_type =loss_fn, cauchy_scale=0.420, algo_name=algo)
+                batch_size=batch_size, reparam_noise=1e-6, reward_scale=scale, loss_type =loss_fn, 
+                cauchy_scale=0.420, stoch=sdist, algo_name=algo, erg=ergodicity)
 
-    n_rounds = 1    # number of trials
-    n_steps = 3e6    # maximum cumulative steps per round
-    n_games = 300    # maximum number of episodes per round
-
-    directory = 'results/'+env_id+'/'+env_id+'_'+algo+'_'+loss_fn+'_'+'e'+str(n_games)+'_'+'n'
-
-    for itr in range(n_rounds):
-        # set intial best to worst possible reward
-        best_score = env.reward_range[0]
+    for round in range(n_trials):
+        best_score = env.reward_range[0] # set intial best to worst possible reward
         score_history = []
-        step_history = np.zeros((n_games,), dtype=np.uint32)
-        loss_history = np.zeros((n_games, 3))
-        scale_history = np.zeros((n_games, 2))
-
-        if algo == 'SAC':
-            logtemp_history = np.zeros((n_games,))
-
+        trial_log = np.zeros((n_episodes+1, 9))
         cum_steps = 0
+        epis = 0
 
         # agent.load_models()
 
-        # while cum_steps < n_steps:
-        for i in range(n_games):
+        while epis <= (n_episodes): # or cum_steps <= n_cumsteps:
+            start_time = time.perf_counter()
+
             state = env.reset()
             done = False
 
@@ -76,51 +69,60 @@ if __name__ == '__main__':
 
             while not done:
                 # action = env.action_space.sample()
-                action = agent.select_next_action(state)
+                action = agent.select_next_action(state, multi='no')
                 next_state, reward, done, info = env.step(action)
-                agent.store_transistion(state, action, reward, next_state, done)
-                q1_loss, q2_loss, actor_loss, scale_1, scale_2, logtemp = agent.learn(loss_type=loss_fn)
-                state = next_state
 
+                batch_states, batch_actions, batch_rewards, \
+                                    batch_next_states, batch_dones = agent.get_mini_batch(batch_size)
+
+                # print(batch_states)
+                bat_targets = agent.multi_step_target(batch_actions, batch_rewards, batch_next_states, \
+                                                      batch_dones, env, multi_steps, ergodicity)
+                # print(bat_targets)
+                q1_loss, q2_loss, actor_loss, scale_1, scale_2, logtemp = \
+                            agent.learn(batch_states, batch_actions, bat_targets, loss_fn, ergodicity)
+
+                agent.store_transistion(state, action, reward, next_state, done)
+
+                # env.reset()                                                               
+                env.state = next_state
+
+                state = next_state
                 score += reward
                 step += 1
                 cum_steps += 1
                 # print(step)
+
                 # env.render(mode='human')
+
+            # env.close()
+            end_time = time.perf_counter()
 
             score_history.append(score)
             trail_score = np.mean(score_history[-100:])
-            step_history[i] = step
-            loss_history[i] = np.array([q1_loss, q2_loss, actor_loss])
-            scale_history[i] = np.array([scale_1, scale_2])
-            
-            if algo == 'SAC':
-                logtemp_history[i] = logtemp
-
-            print('epis/step/tot {}/{}/{}: score {:1.1f}, trail {:1.1f}, C/A loss {:1.1f}/{:1.1f}'
-                .format(i+1, step, cum_steps, score, trail_score, q1_loss+q2_loss, actor_loss))
-
+            trial_log[epis, 0], trial_log[epis, 2], trial_log[epis, 8] = end_time - start_time, step, logtemp
+            trial_log[epis, 3:6] = np.array([q1_loss, q2_loss, actor_loss])
+            trial_log[epis, 6:8] = np.array([scale_1, scale_2])
+             
             if trail_score > best_score:
                 best_score = trail_score
                 agent.save_models()
 
-        filename_npy_score = directory + str(itr+1)+'_score'+'.npy'
-        filename_npy_step = directory + str(itr+1)+'_step'+'.npy'
-        filename_npy_loss = directory + str(itr+1)+'_loss'+'.npy'
-        filename_npy_scale = directory + str(itr+1)+'_scale'+'.npy'
-        filename_npy_logtemp = directory + str(itr+1)+'_logtemp'+'.npy'
-        filename_png = directory + str(itr+1)+'.png'
+            print('eps/step/tot {}/{}/{}: score {:1.1f}, trail {:1.0f}, C/A loss {:1.1f}/{:1.1f}, min {:1.1f}'
+                .format(epis+1, step, cum_steps, score, trail_score, \
+                        q1_loss+q2_loss, actor_loss, (end_time-start_time)/60))
+            
+            epis += 1
+
+        trial_log[:, 1] = np.array(score_history)
+
+        directory = 'results/'+env_id+'/'+env_id+'_'+algo+'_'+loss_fn+'_'+'e'+str(n_episodes)+'_'+'n'
+        filename_npy = directory + str(round+1)+'_log'+'.npy'
+        filename_png = directory + str(round+1)+'.png'
 
         if not os.path.exists('./results/'+env_id):
             os.makedirs('./results/'+env_id)
-        
-        score_history = np.array(score_history)
-        np.save(filename_npy_score, score_history)
-        np.save(filename_npy_step, step_history)
-        np.save(filename_npy_loss, loss_history)
-        np.save(filename_npy_scale, scale_history)
-        
-        if algo == 'SAC':
-            np.save(filename_npy_logtemp, logtemp_history)
 
-        plot_learning_curve(score_history, filename_png)
+        np.save(filename_npy, trial_log)
+
+        plot_learning_curve(score_history[1:], filename_png)
